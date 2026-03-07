19d35295aaeb1a9a5603d3d151b5576ee308e77f
lzzwannasleep <2948088425@qq.com>
2026-03-08 02:23:12 +0800

    feat: hash.TwoSum accepted

---
+++ src/main/java/com/itlzz/hash/TwoSum.java
@@ -0,0 +1,18 @@
+package com.itlzz.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(hashtable.containsKey(target - nums[i])){
+                return new int[]{i, hashtable.get(target - nums[i])};
+            }else{
+                hashtable.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+}
