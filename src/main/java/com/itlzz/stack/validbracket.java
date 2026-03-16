04510efe7c7a474dcdf123c671513a2d35bc0cf5
lzzwannasleep <2948088425@qq.com>
2026-03-17 01:14:25 +0800

    feat: stack.validbracket accepted; UnitTests added

---
+++ src/main/java/com/itlzz/stack/validbracket.java
@@ -0,0 +1,25 @@
+package com.itlzz.stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class validbracket {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char c2 = stack.pop();
+                if((c == ')' && c2 != '(') || (c == '}' && c2 != '{') || (c == ']' && c2 != '[')){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
---
+++ src/test/java/com/itlzz/LeetCodeHot100Tests.java
@@ -0,0 +1,12 @@
+package com.itlzz;
+
+
+import org.junit.jupiter.api.Test;
+
+public class LeetCodeHot100Tests {
+
+    @Test
+    public void test(){
+
+    }
+}
