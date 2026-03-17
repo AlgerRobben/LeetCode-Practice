3d928660183ecbeca163392222a3ed11a6bca718
lzzwannasleep <2948088425@qq.com>
2026-03-17 19:48:28 +0800

    feat: //leetcode 2.两数相加开题;

---
+++ src/main/java/com/itlzz/linkedlist/AddTwoNums.java
@@ -0,0 +1,10 @@
+package com.itlzz.linkedlist;
+
+//leetcode 2.两数相加
+
+public class AddTwoNums {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return new ListNode();
+    }
+}
