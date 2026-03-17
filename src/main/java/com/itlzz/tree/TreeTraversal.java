d0fb304e2e1ec3d1f4b511d944469664e4ef6ac1
lzzwannasleep <2948088425@qq.com>
2026-03-17 15:41:04 +0800

    feat: linkedlist.TreeTraversal accepted

---
+++ src/main/java/com/itlzz/tree/TreeTraversal.java
@@ -0,0 +1,47 @@
+package com.itlzz.tree;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TreeTraversal {
+
+    //递归写法
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> lst = new LinkedList<>();
+        if(root == null){
+            return lst;
+        }else {
+            lst.addAll(inorderTraversal(root.left));
+            lst.add(root.val);
+            lst.addAll(inorderTraversal(root.right));
+        }
+        return lst;
+    }
+
+    //遍历写法利用栈
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> lst = new LinkedList<>();
+        if(root == null){
+            return lst;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode curNode;
+        stack.push(root);
+        while(!stack.isEmpty()){
+            curNode = stack.peek();
+            if (curNode.left != null){
+                stack.push(curNode.left);
+                //入栈后断开，以防重复进栈
+                curNode.left = null;
+            }else {
+                lst.add(stack.pop().val);
+                if(curNode.right != null){
+                    stack.push(curNode.right);
+                    curNode.right = null;
+                }
+            }
+        }
+        return lst;
+    }
+}
