de76a8a15e7ad320f87bd945e9bcfe6397c713eb
lzzwannasleep <2948088425@qq.com>
2026-03-17 10:28:29 +0800

    feat: dp.YangHuiTriangle accepted

---
+++ src/main/java/com/itlzz/dp/YangHuiTriangle.java
@@ -0,0 +1,24 @@
+package com.itlzz.dp;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class YangHuiTriangle {
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new LinkedList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new LinkedList<>();
+            for (int j = 0; j < i + 1; j++) {
+                if(j == 0 || j == i){
+                    row.add(1);
+                }else {
+                    row.add(triangle.get(i - 1).get(j) + triangle.get(i - 1).get(j - 1));
+                }
+            }
+            triangle.add(row);
+        }
+        return triangle;
+    }
+
+}
